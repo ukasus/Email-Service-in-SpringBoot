659880f5488590c1f7760e30f71a56a2ab074f10
ukasus <ujjawal98kaushik@gmail.com>
2020-08-03 13:35:36 +0530

    Configuration Change

--- src/test/java/com/ujjawal/projects/HareKrishnaConfectionryApplicationTests.java
+++ src/test/java/com/ujjawal/projects/HareKrishnaConfectionryApplicationTests.java
@@ -1,20 +1,9 @@
 package com.ujjawal.projects;
 
-import java.io.UnsupportedEncodingException;
-
-import javax.mail.internet.InternetAddress;
-
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import com.google.common.collect.Lists;
 
-import it.ozimov.springboot.mail.model.Email;
-import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
+
 
 
 
