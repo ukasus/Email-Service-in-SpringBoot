7b586de16d350672e829d519a4b403c9eda9aa8c
ukasus <ujjawal98kaushik@gmail.com>
2020-08-03 13:18:17 +0530

    Configuration Change

--- src/test/java/com/ujjawal/projects/HareKrishnaConfectionryApplicationTests.java
+++ src/test/java/com/ujjawal/projects/HareKrishnaConfectionryApplicationTests.java
@@ -17,23 +17,8 @@ import it.ozimov.springboot.mail.model.Email;
 import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
 
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 class HareKrishnaConfectionryApplicationTests {
-    @Autowired 
-    it.ozimov.springboot.mail.service.EmailService emailService; 
 
-    @Value("${spring.mail.username}") String fromEmail; 
-    @Test 
-    public void testSendEmail() throws UnsupportedEncodingException { 
-        
-        final Email email = DefaultEmail.builder() 
-            .from(new InternetAddress(fromEmail, "HareKrishnaConfectionary"))
-            .to(Lists.newArrayList(new InternetAddress(
-                    "ujjawal98kaushik@gmail.com", "Ujjawal Sharma"))) 
-            .subject("Testing email")
-            .body("Testing body ...")
-            .encoding("UTF-8").build();
-        emailService.send(email); 
-    }
 }
